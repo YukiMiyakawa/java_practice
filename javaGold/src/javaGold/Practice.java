dd150f1e08b7c0011c80350124e03a22468f16ef
YukiMiyakawa <sakomiya3838@gmail.com>
2022-08-03 21:46:30 +0900

    jar test

---
+++ javaGold/src/javaGold/Practice.java
@@ -0,0 +1,12 @@
+package javaGold;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+
+public class Practice {
+	String name;
+	String address;
+	
+	public boolean equals(Object o) {
+		return EqualsBuilder.reflectionEquals(this, o);
+	}
+}
